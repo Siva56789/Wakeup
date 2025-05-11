298284d189ee9743a7f563be56597aab3d506a90
Siva56789 <siva.enuganti@gmail.com>
2025-05-11 15:01:32 +0530

    First code

---
+++ src/test/java/org/sdet268batch/P.java
@@ -0,0 +1,14 @@
+package org.sdet268batch;
+
+public class P {
+
+	public static void main(String[] args) {
+		
+		
+		String s ="Siva is automation engieer";
+		System.out.println(s);
+		
+		// code 
+	}
+
+}
